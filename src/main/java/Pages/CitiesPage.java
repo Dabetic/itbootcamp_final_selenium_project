2d966db9db1fa17f9d2a9de63b88735f36b5bea4
Milos Dabetic <milos.dabetic@gmail.com>
2023-10-18 18:48:49 +0200

    Cities page updated, admin cities tests, test5 created

--- src/main/java/Pages/CitiesPage.java
+++ src/main/java/Pages/CitiesPage.java
@@ -30,7 +30,24 @@ public class CitiesPage extends BasicPage{
     }
 
     public void waitUntilTrSizeIsOne () {
-        wait.until(ExpectedConditions.numberOfElementsToBe(By.cssSelector("tbody tr"),1));
+        wait.withMessage("Actual number of elements doesn't match expected number of elements")
+                .until(ExpectedConditions.numberOfElementsToBe
+                        (By.cssSelector("tbody tr .text-left:nth-child(2)"),1));
     }
+
+    public String tableDataNameFromTr () {
+        List<WebElement>tableRowElements = getCitiesTrNumber();
+        String cityName = null;
+        for (int i = 0; i < tableRowElements.size(); i++) {
+            cityName = tableRowElements.get(i).findElement(By.cssSelector(".text-left:nth-child(2)")).getText();
+        }
+        return cityName;
+    }
+
+    public boolean isContaisMilosCityTxt () {
+      return tableDataNameFromTr().contains("Milos Dabetic's City");
+    }
+
+
 
 }
--- src/test/java/Tests/AdminCitiesTests.java
+++ src/test/java/Tests/AdminCitiesTests.java
@@ -83,7 +83,25 @@ public class AdminCitiesTests extends BasicTest{
                 "Saved successfully",
                 "Actual pop up message doesn't match expected pop up message");
     }
+    @Test (priority = 5, retryAnalyzer = RetryAnalyzer.class)
+    public void searchACity () {
+        String email = "admin@admin.com";
+        String password = "12345";
+        navPage.getLoginBtn().click();
+        loginPage.getEmailInput().clear();
+        loginPage.getEmailInput().sendKeys(email);
+        loginPage.getPasswordInput().clear();
+        loginPage.getPasswordInput().sendKeys(password);
+        loginPage.getLoginBtn().click();
+        navPage.getAdminBtn().click();
+        navPage.getCitiesBtn().click();
 
+        citiesPage.getSearchCityBtn().sendKeys("Milos Dabetic's City");
 
+        citiesPage.waitUntilTrSizeIsOne();
+
+        Assert.assertTrue(citiesPage.isContaisMilosCityTxt());
+
+    }
 
 }
